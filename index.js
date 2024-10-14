a903e5471be381ba6a0ed1dac445d1b4679fe7ab
Lise PERARD <liseprd@hotmail.fr>
2024-10-14 17:03:09 +0200

    création de la vache

---
+++ index.js
@@ -0,0 +1,12 @@
+const cowsay = require("cowsay");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const name = process.env.NAME;
+const campus = process.env.CAMPUS;
+
+console.log(
+	cowsay.say({
+		text: `I am ${name}, and I'm on the campus of ${campus}.`,
+	}),
+);
